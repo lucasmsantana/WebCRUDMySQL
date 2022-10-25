145ba0a806402bc513ba889da9ce623368584266
Lucas Santana <lucasantana78@gmail.com>
2022-10-25 19:21:55 -0300

    Web CRUD e tabela criada

---
+++ WebCrudMySQL/Context/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebCrudMySQL.Models;
+
+namespace WebCrudMySQL.Context
+{
+    public class Contexto : DbContext
+    {
+
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options)
+        {
+
+        }
+        
+        public DbSet<Usuario> Usuario { get; set; }
+        
+    }
+}
---
+++ WebCrudMySQL/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebCrudMySQL.Models
+{
+    [Table("usuario")]
+    public class Usuario
+    {
+        [Display(Name ="id")]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Display(Name = "nome")]
+        [Column("nome")]
+        public string Nome { get; set; }
+    }
+}
